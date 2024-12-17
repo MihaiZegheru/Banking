238aeb5a681e1c7a1654b51062d669bd5c35e3ee
MihaiZegheru <mihai.z.2005@gmail.com>
2024-12-17 17:19:43 +0200

    Added ZeroPaymentreceiver comment.
    Signed-off-by: MihaiZegheru <mihai.z.2005@gmail.com>

--- src/main/java/org/poo/banking/transaction/ZeroPaymentReceiver.java
+++ src/main/java/org/poo/banking/transaction/ZeroPaymentReceiver.java
@@ -1,5 +1,8 @@
 package org.poo.banking.transaction;
 
+/**
+ * In case you would like to make a void payment. (Tests only)
+ */
 public class ZeroPaymentReceiver implements PaymentReceiver {
     @Override
     public void receive(final double amount, final String currency) { }
